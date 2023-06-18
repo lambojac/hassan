dad3a8cbd68ff006ef12b7d5b4959c63f2c191de
adamokennie <adamokennie@gmail.com>
2023-06-18 17:45:29 +0100

    commit made successfully

--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect("mongodb://localhost:27017/bookingsRegistration",{
+mongoose.connect("mongodb+srv://kennie:869480Ak@cluster0.zkjbfkp.mongodb.net/?retryWrites=true&w=majority",{
 
 }).then(()=>{
     console.log(`connection successful`);
